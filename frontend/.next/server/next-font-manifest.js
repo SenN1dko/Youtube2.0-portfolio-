ccc18d0120e93af27cc43e8b6bec813391d65eaa
senn1sd <amirraimovtop1@gmail.com>
2026-04-17 02:12:19 +0500

    added a .env.example file

---
+++ frontend/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/senn1sd/MyProjets/Youtube2.0-portfolio-/frontend/src/app/layout\":[\"static/media/1755441e3a2fa970-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
