36de41dd8654d063e97fa514bb754cb6a81a4b17
senn1sd <amirraimovtop1@gmail.com>
2026-05-19 21:55:21 +0500

    [fix] Merge fix

--- frontend/src/components/layout/content/Header/HeaderProfile.tsx
+++ frontend/src/components/layout/content/Header/HeaderProfile.tsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { STUDIO_PAGE } from "@/config/studio-page.config";
-import Image from "next/image";
-import Link from "next/link";
-
-export function HeaderProfile() {
-    return <Link href={STUDIO_PAGE.SETTINGS} className="shrink-0">
-    <Image
-    alt='Profile'
-    src={'/avatar.png'}
-    width={40}
-    height={40}
-    className='rounded-lg '
-    />
-    </Link>
-=======
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -27,23 +10,6 @@ import { useAuthStore } from '@/store'
 
 export function HeaderProfile() {
 	const isLoggedIn = useAuthStore(set => set.isLoggedIn)
-=======
-import Image from 'next/image'
-import Link from 'next/link'
-
-import { LinkButton } from '@/ui/button/LinkButton'
-
-import { PAGE } from '@/config/public-page.config'
-import { STUDIO_PAGE } from '@/config/studio-page.config'
-
-import { authservice } from '@/services/auth.services'
-import { useAuthStore } from '@/store'
-
-export function HeaderProfile() {
-	const user = useAuthStore(set => set.user)
-	const isLoggedIn = useAuthStore(set => set.isLoggedIn)
-	console.log(isLoggedIn)
->>>>>>> 20ea2b2d ([+]Added store for setUser and logout)
 	return isLoggedIn ? (
 		<>
 			<Link
@@ -57,27 +23,11 @@ export function HeaderProfile() {
 					height={40}
 					className='rounded-lg '
 				/>
-<<<<<<< HEAD
-=======
-				<span className='w-10 h-4 text-black'>{user?.email}</span>
->>>>>>> 20ea2b2d ([+]Added store for setUser and logout)
 			</Link>
 		</>
 	) : (
 		<>
 			<LinkButton href={PAGE.AUTH}>Auth</LinkButton>
-<<<<<<< HEAD
 		</>
 	)
->>>>>>> 88b3b0f0 ([+] added login and logout buttons)
-=======
-			<span
-				onClick={() => authservice.logout}
-				className='w-10 h-4 text-black'
-			>
-				{user?.id}
-			</span>
-		</>
-	)
->>>>>>> 20ea2b2d ([+]Added store for setUser and logout)
 }
